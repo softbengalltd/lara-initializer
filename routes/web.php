471f9357ba68c125338e8f998cb28f9a6d5bfb6f
engrshishir <shishir.cse.pstu@gmail.com>
2025-03-15 05:28:57 +0600

    .

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use EngrShishir\Larainitializer\Http\Controllers\LarainitializerController;
+use Softbengalltd\Larainitializer\Http\Controllers\LarainitializerController;
 
 Route::middleware(['guest','web'])->group(function(){
     Route::get('/setup/{step}', [LarainitializerController::class, 'create'])->name('squartup.setup.form');
